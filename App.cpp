445c143e6b85a5faf0443b196e9b64f40f24240b
Jacek Popławski <jacek.poplawski@nokia.com>
2021-11-29 21:33:01 +0100

    autformat App.cpp

--- App.cpp
+++ App.cpp
@@ -1,4 +1,5 @@
 #include "App.h"
+
 #include <sstream>
 
 constexpr size_t hash(const char* str)
@@ -19,7 +20,8 @@ App::App()
 {
     std::srand(time(0));
     contest = new Contest(0.0f, std::time(0), 1);
-    std::cout << "\n --- *** SECRET SANTA MK II *** ---\n Type \"help\" to show command list. Pass an empty line, or type \"quit\" in order to exit.\n\n";
+    std::cout << "\n --- *** SECRET SANTA MK II *** ---\n Type \"help\" to show command list. Pass an empty line, or "
+                 "type \"quit\" in order to exit.\n\n";
 }
 
 int App::getInput()
@@ -31,12 +33,20 @@ int App::getInput()
     std::stringstream stream(buffer);
     std::getline(stream, token, ' ');
 
-    if (!buffer[0]) return 0;
+    if (!buffer[0])
+        return 0;
 
     switch (hash(&token[0]))
     {
         case hash("help"):
-            std::cout << ">add NAME\n -add a participant to the contest, each one must have a uniqe NAME\n>set prize NUMBER\n>set winners NUMBER\n -change amount of possible winners (one by default)\n>set deadline YEAR/MONTH/DAY (date has to be all numeric with slashes)\n>prize\n -show the current prize\n>deadline\n -show the current deadline\n>find NAME\n -show if the NAME is a participant\n>participants\n -show the lists of participants\n>winners\n -show the list of winners\n>roll\n -randomly choose a set amount of winners from participants\n>remove NUMBER or remove NAME\n -remove a participant from the contest by his NAME or his NUMBER on the list\n>clear\n -reset the contest\n>quit\n";
+            std::cout << ">add NAME\n -add a participant to the contest, each one must have a uniqe NAME\n>set prize "
+                         "NUMBER\n>set winners NUMBER\n -change amount of possible winners (one by default)\n>set "
+                         "deadline YEAR/MONTH/DAY (date has to be all numeric with slashes)\n>prize\n -show the "
+                         "current prize\n>deadline\n -show the current deadline\n>find NAME\n -show if the NAME is a "
+                         "participant\n>participants\n -show the lists of participants\n>winners\n -show the list of "
+                         "winners\n>roll\n -randomly choose a set amount of winners from participants\n>remove NUMBER "
+                         "or remove NAME\n -remove a participant from the contest by his NAME or his NUMBER on the "
+                         "list\n>clear\n -reset the contest\n>quit\n";
             break;
 
         case hash("add"):
@@ -54,25 +64,15 @@ int App::getInput()
             contest->findParticipant(token);
             break;
 
-        case hash("participants"):
-            contest->showParticipants();
-            break;
+        case hash("participants"): contest->showParticipants(); break;
 
-        case hash("winners"):
-            contest->showWinners();
-            break;
+        case hash("winners"): contest->showWinners(); break;
 
-        case hash("roll"):
-            contest->rollWinners();
-            break;
+        case hash("roll"): contest->rollWinners(); break;
 
-        case hash("deadline"):
-            contest->showDeadline();
-            break;
+        case hash("deadline"): contest->showDeadline(); break;
 
-        case hash("prize"):
-            contest->showPrize();
-            break;
+        case hash("prize"): contest->showPrize(); break;
 
         case hash("remove"):
             getline(stream, token, '\n');
@@ -86,16 +86,11 @@ int App::getInput()
             }
             break;
 
-        case hash("clear"):
-            contest->clear();
-            break;
+        case hash("clear"): contest->clear(); break;
 
-        case hash("quit"):
-            return 0;
+        case hash("quit"): return 0;
 
-        default:
-            std::cout << ">invalid command" << std::endl;
-            break;
+        default: std::cout << ">invalid command" << std::endl; break;
     }
 
     std::cout << std::endl;
@@ -106,7 +101,8 @@ int App::stringIsNumber(std::string& string)
 {
     for (auto letter : string)
     {
-        if (!std::isdigit(letter)) return 0;
+        if (!std::isdigit(letter))
+            return 0;
     }
 
     return 1;
@@ -122,7 +118,7 @@ void App::passSetCommand(std::stringstream& stream, std::string& token)
             {
                 contest->setPrize(std::stof(token));
             }
-            else 
+            else
             {
                 std::cout << ">invalid value";
             }
@@ -145,12 +141,15 @@ void App::passSetCommand(std::stringstream& stream, std::string& token)
             int year = 1900, month = 1, day = 1;
 
             std::getline(stream, token, '/');
-            if (stringIsNumber(token)) year = std::stoi(token);
+            if (stringIsNumber(token))
+                year = std::stoi(token);
             std::getline(stream, token, '/');
-            if (stringIsNumber(token)) month = std::stoi(token);
+            if (stringIsNumber(token))
+                month = std::stoi(token);
             std::getline(stream, token, '/');
-            if (stringIsNumber(token)) day = std::stoi(token);
- 
+            if (stringIsNumber(token))
+                day = std::stoi(token);
+
             if (year > 1900 && month > 0 && month < 13 && day > 0 && day < 32)
             {
                 contest->setDeadline(year, month, day);
